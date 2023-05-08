91255202962946ed5da8727c60d437fb164f70f3
koimasshiro <miss.eselawrencegmail.com>
2023-05-08 18:47:22 +0100

    added more styling to benefits section

--- src/Components/Benefits/Benefits.jsx
+++ src/Components/Benefits/Benefits.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import "./Benefits.css";
-import FirstTab from "./Tabs/FirstTab";
-import SecondTab from "./Tabs/SecondTab";
-import ThirdTab from './Tabs/ThirdTab';
+import FirstTab from "./Tabs/FirstTab/FirstTab";
+import SecondTab from "./Tabs/SecondTab/SecondTab";
+import ThirdTab from "./Tabs/ThirdTab/ThirdTab";
 
 const Benefits = () => {
   const [activeTab, setActiveTab] = useState("tab1");
@@ -18,21 +18,43 @@ const Benefits = () => {
   };
   const handleTab3 = () => {
     //update the state to tab3
-    setActiveTab('tab3')
-  }
+    setActiveTab("tab3");
+  };
 
   return (
     <div>
       <h2 style={{ textAlign: "center" }}>Benefits</h2>
+      {/* Tab nav */}
+      <ul className="nav">
+        <li
+          className={activeTab === "tab1" ? "active" : ""}
+          onClick={handleTab1}
+        >
+          Benefits to participants
+        </li>
+        <li
+          className={activeTab === "tab2" ? "active" : ""}
+          onClick={handleTab2}
+        >
+          Benefits to mentors
+        </li>
+        <li
+          className={activeTab === "tab3" ? "active" : ""}
+          onClick={handleTab3}
+        >
+          Benefits to Nigeria
+        </li>
+      </ul>
       <div className="Tabs">
-        {/* Tab nav */}
-        <ul className="nav">
-          <li className={activeTab === "tab1" ? "active" : ""} onClick={handleTab1} >Benefits to participants</li>
-          <li className={activeTab === "tab2" ? "active" : ""} onClick={handleTab2} >Benefits to mentors</li>
-          <li className={activeTab === "tab3" ? "active" : ""} onClick={handleTab3} >Benefits to Nigeria</li>
-        </ul>
         <div className="outlet">
-          {activeTab === "tab1" ? <FirstTab /> : <SecondTab /> && activeTab === 'tab3' ? <ThirdTab/> : <SecondTab/>}
+          {activeTab === "tab1" ? (
+            <FirstTab />
+          ) : <SecondTab /> && activeTab === "tab3" ? (
+            <ThirdTab />
+          ) : 
+          (
+            <SecondTab />
+          )}
         </div>
       </div>
     </div>
