95dfb2224dae00cb350c8162f21504b8341d6cbd
koimasshiro <miss.eselawrencegmail.com>
2023-05-09 10:07:04 +0100

    Complete styling for benefits section

--- src/Components/Benefits/Benefits.jsx
+++ src/Components/Benefits/Benefits.jsx
@@ -1,64 +1,122 @@
 import React, { useState } from "react";
 import "./Benefits.css";
-import FirstTab from "./Tabs/FirstTab/FirstTab";
-import SecondTab from "./Tabs/SecondTab/SecondTab";
-import ThirdTab from "./Tabs/ThirdTab/ThirdTab";
 
 const Benefits = () => {
-  const [activeTab, setActiveTab] = useState("tab1");
-
-  //  Functions to handle Tab Switching
-  const handleTab1 = () => {
-    // update the state to tab1
-    setActiveTab("tab1");
-  };
-  const handleTab2 = () => {
-    // update the state to tab2
-    setActiveTab("tab2");
-  };
-  const handleTab3 = () => {
-    //update the state to tab3
-    setActiveTab("tab3");
-  };
-
   return (
-    <div>
-      <h2 style={{ textAlign: "center" }}>Benefits</h2>
-      {/* Tab nav */}
-      <ul className="nav">
-        <li
-          className={activeTab === "tab1" ? "active" : ""}
-          onClick={handleTab1}
-        >
-          Benefits to participants
-        </li>
-        <li
-          className={activeTab === "tab2" ? "active" : ""}
-          onClick={handleTab2}
-        >
-          Benefits to mentors
-        </li>
-        <li
-          className={activeTab === "tab3" ? "active" : ""}
-          onClick={handleTab3}
-        >
-          Benefits to Nigeria
-        </li>
-      </ul>
-      <div className="Tabs">
-        <div className="outlet">
-          {activeTab === "tab1" ? (
-            <FirstTab />
-          ) : <SecondTab /> && activeTab === "tab3" ? (
-            <ThirdTab />
-          ) : 
-          (
-            <SecondTab />
-          )}
+    <>
+      <h2 style={{ paddingBottom: "20px" }}>Benefits</h2>
+      <div class="wrapper">
+        <input className="radio" id="one" name="group" type="radio" checked />
+        <input className="radio" id="two" name="group" type="radio" />
+        <input className="radio" id="three" name="group" type="radio" />
+
+        <div class="tabs">
+          <label className="tab" id="one-tab" for="one">
+            Benefits to participants
+          </label>
+          <label className="tab" id="two-tab" for="two">
+            Benefits to Mentors
+          </label>
+          <label className="tab" id="three-tab" for="three">
+            Benefits to Nigeria
+          </label>
+        </div>
+
+        <div className="panels">
+          <div className="panel" id="one-panel">
+            <div>
+              <h4 className="panel-title">
+                {" "}
+                Successful applicants are entitled to the following benefits:{" "}
+              </h4>
+            </div>
+            <ul className="benefits-list">
+              <li>
+                A unique platform to transform ideas into inventions and develop
+                working prototypes.
+              </li>
+              <li>
+                Access to a special certificate course and mentoring by
+                accomplished inventors, innovators, and researchers from Nigeria
+                and the State of Israel.
+              </li>
+              <li>
+                Free access and membership to a modern innovation center where
+                they can work on and develop their ideas.
+              </li>
+              <li>The opportunity to enter “Mass Challenge Israel, 2022”.</li>
+              <li>
+                The opportunity to pitch inventions and prototypes to investors
+                for possible adoption, patronage and partnership.
+              </li>
+            </ul>
+          </div>
+          <div class="panel" id="two-panel">
+            <ul className="benefits-list">
+              <li>Automatic adoption as a Fellow of the Innov8 Center.</li>
+              <li>
+                Enlistment into i-FAIR database of Mentors, for references and
+                future engagements.
+              </li>
+              <li>
+                Opportunity to network with fellow mentors from other parts of
+                the world, especially Israel and Nigeria.
+              </li>
+              <li>
+                Opportunity to network with renowned as well as aspiring
+                Inventors, Innovators and Researchers in Mentor’s field of
+                expertise.
+              </li>
+              <li>
+                Access to New Innovations, Inventions and Researches in Mentor’s
+                field of expertise.
+              </li>
+              <li>
+                Access to an interactive platform where i-FAIR Organizers,
+                Sponsors, Partners, Participants, Mentors and Investors will
+                meet and interact.
+              </li>
+              <li>i-FAIR Mentorship Certificate Award to Mentors.</li>
+            </ul>
+          </div>
+          <div class="panel" id="three-panel">
+            <ul className="benefits-list">
+              <li>
+                Development of innovations with the potential to address
+                challenges in the Nigerian society.
+              </li>
+              <li>
+                Development of Made-In-Nigeria solutions to challenges in
+                Nigeria.
+              </li>
+              <li>
+                Economic advancement and wealth creation by way of venture
+                creation.
+              </li>
+              <li>
+                Job creation through innovation development, by the transition
+                of innovations into start-ups.
+              </li>
+              <li>
+                Introducing Nigerians to technologies, processes and procedures
+                with the capability to solve critical challenges
+              </li>
+              <li>
+                Creating self-sufficiency and productivity within the Nigerian
+                innovation ecosystem.
+              </li>
+              <li>
+                Nation-building through innovation, Research & Development, and
+                entrepreneurship.
+              </li>
+              <li>
+                Putting Nigeria on the path to becoming a start-up nation.
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
-
 export default Benefits;
